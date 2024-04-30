e36f6e83e7e2a982dba44c4ab5a466076e9b5f52
Sahil Khachi <sahilkhachi95@gmail.com>
2024-05-01 00:11:30 +0530

    commit

--- Frontend/recoil/atoms.js
+++ Frontend/recoil/atoms.js
@@ -1,9 +1,41 @@
-import {atom} from 'recoil';
+import { atom,selector,useRecoilState, useRecoilValue  } from 'recoil';
+import axios from 'axios';
 
 
-const isClient = atom({
-    key: 'isClient', 
-    default: 'sahil', 
-  });
+export const isClient = atom({
+  key: 'isClient',
+  default: 'sahil',
+});
 
-  export { isClient }; 
+export const userDataAtom = atom({
+  key: 'userData',
+  default: null,
+});
+
+export const fetchUserDataSelector = selector({
+  key: 'fetchUserData',
+  get: async ({ get }) => {
+    try {
+      const response = await axios.get('http://localhost:3000/empmng/admin/bulk');
+      return response.data; 
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      throw error;
+    }
+  },
+});
+
+export const useFetchAndUpdateUserData = () => {
+  const [userData, setUserData] = useRecoilState(userDataAtom);
+
+  const fetchDataAndUpdateAtom = async () => {
+    try {
+      const fetchedUserData = await fetchUserDataSelector();
+      setUserData(fetchedUserData);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  return fetchDataAndUpdateAtom;
+};
--- Frontend/src/SmallComponent/Dashboard/MainComponent.jsx
+++ Frontend/src/SmallComponent/Dashboard/MainComponent.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Card from './Card';
 import { useRecoilValue } from 'recoil';
-import { isClient } from '../../../recoil/atoms';
+import { isClient,useFetchAndUpdateUserData,userDataAtom } from '../../../recoil/atoms';
 
 const MainContent = ({ sidebarOpen }) => {
+    const fetchData = useFetchAndUpdateUserData();
     const Client = useRecoilValue(isClient);
+    const data = useRecoilValue(userDataAtom);
+
     if (!Client) {
         return <Navigate to="/" />;
     }
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+    console.log(data)
     return (
         <div className={`flex flex-col  top-16 bg-gray-100 justify-center items-center h-screen p-0 bg-white dark:bg-gray-600 ${sidebarOpen ? 'ml-64' : 'sm:ml-64'}`}>
             <Card />
